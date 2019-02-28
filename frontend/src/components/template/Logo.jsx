3f9969525e2cb1b173821708d101dc8ab76e6de5
Gustavo <gustavofabro.f@gmail.com>
2019-02-27 22:00:48 -0300

    update styles

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,7 +1,11 @@
 import './Logo.css'
+import logo from '../../assets/imgs/logo.png'
+
 import React from 'react'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <a href="/" className="logo">
+            <img src={logo} alt="logo"></img>
+        </a>
     </aside>
