5fea2dcb1883cd8649b4b91004e8bfee838e7a6c
Gustavo <gustavofabro.f@gmail.com>
2019-02-28 22:21:24 -0300

    implemented components

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" titulo="Inicío"
+        subtitle="Segundo projeto react">
+
+        <div className="display-4">Bem vindo</div>
+        <hr />
+        <p className="mb-0">Sistema de crud em React</p>
+    </Main>
