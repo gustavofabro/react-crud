8965728e290b88cc16f4c49c6b3686c30dca57e9
Gustavo <gustavofabro.f@gmail.com>
2019-03-05 17:18:57 -0300

    Routes

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,11 +1,12 @@
 import './Logo.css'
 import logo from '../../assets/imgs/logo.png'
+import { Link } from 'react-router-dom'
 
 import React from 'react'
 
 export default props =>
     <aside className="logo">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img src={logo} alt="logo"></img>
-        </a>
+        </Link>
     </aside>
